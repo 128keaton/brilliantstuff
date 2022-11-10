a7c5b3e82855f6bb484694096c982a330e075644
Keaton Burleson <keaton.burleson@me.com>
2022-11-09 22:49:30 -0600

    Adds awesome icon shit

--- themes/bs-theme/static/js/window-stream.js
+++ themes/bs-theme/static/js/window-stream.js
@@ -1,7 +1,23 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const title = urlParams.get('title');
+let icon = urlParams.get('icon');
 
-document.querySelector('#title').innerHTML =title;
+
+if (!icon) {
+    const icons = [
+        'big-printer.ico',
+        'pens.ico',
+        'printer.png',
+        'replacethispart.ico',
+        'tracer.ico'
+    ];
+
+
+    icon = icons[Math.floor(Math.random() * icons.length)];
+}
+
+document.querySelector('#icon').src = '/img/window-icons/' + icon;
+document.querySelector('#title').innerHTML = title;
 
 
